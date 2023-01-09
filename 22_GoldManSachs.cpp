25a50b059a1978f9eec497710a49304a1f663bc6
samahakal <91861509+samahakal@users.noreply.github.com>
2023-01-09 21:27:05 +0530

    Ordered Set or Segment Tree

--- 22_GoldManSachs.cpp
+++ 22_GoldManSachs.cpp
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/count-good-triplets-in-an-array/description/?orderBy=most_votes
 
+# Complexity
+- Time complexity:O(nlogn)
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:O(n)
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code (ordered_set)
+```
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
@@ -44,3 +53,77 @@ public:
         return ans;       
     }
 };
+
+```
+
+# Code (segment_tree)
+```
+typedef long long ll;
+vector<ll>seg;
+vector<ll>v;
+int n;
+
+
+void build(ll l, ll r, ll c = 1){
+    if(l == r){
+        seg[c] = v[l];
+        return;
+    }
+    ll mid = (l+r)/2;
+    build(l,mid,2*c);
+    build(mid+1,r,2*c+1);
+    seg[c] = seg[2*c]+seg[2*c+1];
+    return;
+}
+
+ll query(ll l,ll r,ll a,ll b,ll c = 1){
+    if(l>b || r<a){
+        return 0;
+    }
+    if(l>=a and r<=b){
+        return seg[c];
+    }
+    ll mid = (l+r)/2;
+    return query(l,mid,a,b,2*c)+query(mid+1,r,a,b,2*c+1);
+}
+
+void update(ll l,ll r, ll ind, ll c=1){
+    if(ind<l || r<ind){
+        return;
+    }
+    if(l == r and l == ind){
+        seg[c] = 1;
+        return;
+    }
+    ll mid = (l+r)/2;
+    update(l,mid,ind,2*c);
+    update(mid+1,r,ind,2*c+1);
+    seg[c] = seg[2*c]+seg[2*c+1];    
+}
+class Solution {
+public:
+    long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        ll n = nums1.size();
+        v.resize(n+1,0);
+        ll ans = 0;
+        map<ll,ll>mp;
+        for(ll i=0;i<n;i++){
+            mp[nums2[i]+1] = i+1;
+        }
+        seg.clear();
+        seg.resize(4*n+1,0);
+        build(1,n);
+        for(int i=1;i<=n;i++){
+            ll r = mp[nums1[i-1]+1];
+            ll q = query(1,n,1,r);
+            ll d = i-q-1;
+            ll e = n-r-d;
+            ans += e*q;
+            update(1,n,r);
+        }
+        return ans;
+    }
+};
+```
+
+# upvote if it's help you HAPPY CODING :)
