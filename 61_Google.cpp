644100966a4cb578c35fa8bf1cbb43a675d80fdd
samahakal <91861509+samahakal@users.noreply.github.com>
2023-01-23 12:25:45 +0530

    Greedy

--- 61_Google.cpp
+++ 61_Google.cpp
@@ -4,20 +4,10 @@ class Solution {
 public:
     int maxConsecutive(int bottom, int top, vector<int>& special) {
         sort(special.begin(),special.end());
-        int ans = 0;
         int n = special.size();
+        int ans = max(special[0]-bottom,top-special[n-1]);
         for(int i=1;i<n;i++){
             ans = max(ans,special[i]-special[i-1]-1);
-        }
-
-
-        int k = lower_bound(special.begin(),special.end(),bottom)-special.begin();
-        if(k<n and special[k]<=top){
-            ans = max(ans,special[k]-bottom);
-        }
-        k = upper_bound(special.begin(),special.end(),top)-special.begin()-1;
-        if(k>=0 and special[k]>=bottom){
-            ans = max(ans,top-special[k]);
         }
 
         return ans;
